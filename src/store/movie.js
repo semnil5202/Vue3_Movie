a43ca4e58d2929799e9362315c9dfb67956bbdc0
SEMNIL5202 <semnil5202@naver.com>
2022-04-01 02:19:12 +0900

    Search Movie Function

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
+
 export default {
     //module
     namespaced: true,
@@ -6,22 +9,44 @@ export default {
         movies: []
     }),
     //computed
-    getters: {
-        movieIds(state) {
-            return state.movies.map(m => m.imdbID)
-        }
-    },
+    getters: {},
     //methods
     //data 변경
     mutations: {
+        updateState(state, payload) {
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key]
+            })
+        },
         resetMovies(state) {
             state.movies = []
         }
     },
     //비동기 방식
     actions: {
-        searchMovies() {
-            
+        async searchMovies({state, commit}, payload) {
+            const { title, type, number, year } = payload
+            const OMDB_API_KEY = '74cd1a89' //1,000days left from 22.03.30
+
+            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+            const { Search, totalResults } = res.data
+            commit('updateState', {
+                movies : _uniqBy(Search, 'imdbID')
+            })
+
+            const total = parseInt(totalResults, 10)
+            const pageLength = Math.ceil(total / 10)
+
+            if (pageLength > 1) {
+                for (let page = 2; page <= pageLength; page++) {
+                    if(page > number / 10) break
+                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+                    const { Search } = res.data
+                    commit('updateState', {
+                        movies : [...state.movies, ..._uniqBy(Search, 'imdbID')]
+                    })
+                }
+            }
         }
     }
 }
