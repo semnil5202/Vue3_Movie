10dbba906b8b6eac83d81c26afadf4fb3d6fe926
SEMNIL5202 <semnil5202@naver.com>
2022-04-04 23:41:37 +0900

    Movie page(route) skeleton and loading anim

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ export default createRouter({
             component: Home
         },
         {
-            path: '/Movie',
+            path: '/movie/:id',
             component: Movie
         },
         {
