ecf7c4a35228bae145546d7b4d604ce1c1b88a0d
SEMNIL5202 <semnil5202@naver.com>
2022-04-07 23:49:52 +0900

    about page

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        image: 'src/assets/mongmong.jpg',
+        name: 'SEMNIL',
+        email: 'semnil5202@naver.com',
+        phone: '+82-10-0000-0000',
+    })
+}
